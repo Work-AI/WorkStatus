bf1c0dc5a0a2f707951b6b7a01522e60f36b8896
Simba Zhang <solderzzc@gmail.com>
2017-05-05 16:52:03 -0700

    To load iohook for event hook.

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,20 @@
 
-console.log('Started')
+const ioHook = require('iohook');
+
+ioHook.on("mousemove", event => {
+    console.log(event);
+});
+
+ioHook.on("keyup", event => {
+    console.log(event);
+});
+ioHook.on("mouseup", event => {
+    console.log(event);
+});
+ioHook.on("mouseclick", event => {
+    console.log(event);
+    //monitor.getActiveWindow(callback);
+});
+//Register and start hook
+ioHook.start();
+
