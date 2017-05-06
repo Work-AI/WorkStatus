3fc74a403354318677086fe5d2f25561cf80f148
Simba Zhang <solderzzc@gmail.com>
2017-05-05 17:32:39 -0700

    Added active-window package into WorkStatus to capture the active window/title.

--- app/js/app.js
+++ app/js/app.js
@@ -18,3 +18,21 @@ ioHook.on("mouseclick", event => {
 //Register and start hook
 ioHook.start();
 
+var monitor = require('active-window');
+
+var previousApp = ''
+var previousTitle = ''
+windowInspector = function(win){
+    try {
+        if(win.app !== previousApp &&  win.title!==previousTitle){
+            console.log("App: " + win.app);
+            console.log("Title: " + win.title);
+            previousApp = win.app
+            previousTitle = win.title
+        }
+    }catch(err) {
+        console.log(err);
+    }
+}
+
+monitor.getActiveWindow(windowInspector,-1,1);
