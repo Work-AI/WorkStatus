e2fb3580047b0095cfe0d48ee4b4620a3674a749
Simba Zhang <solderzzc@gmail.com>
2017-05-12 18:08:32 -0700

    Remove other three watch module usage after save to branch 'save_all_watch_method_example'

--- app/js/filewatcher.js
+++ app/js/filewatcher.js
@@ -2,127 +2,18 @@
  * Created by simba on 5/8/17.
  */
 
-var chokidar = require('chokidar');
 var fs = require('fs');
 var watcher = null;
 var showInLogFlag = false;
-var watch = require('watch');
 var du = require('du');
-var fsmonitor = require('fsmonitor');
 var nodewatch = require('node-watch');
 var reporter = require('./js/logreporter');
 var nexttick = require('next-tick');
+var debounce = require('debounce');
+
 const Configstore = require('configstore');
 const conf = new Configstore('desktop.workai.filewatcher');
 
-function StartWatchWatcher(path){
-    document.getElementById("messageLogger").innerHTML = "Scanning the path, please wait ...";
-    require('du')(path, function (err, size) {
-        console.log('The size of' + path + ' is:', size, 'bytes')
-
-        watch.createMonitor(path,{
-            ignoreDotFiles: true,
-            ignoreUnreadableDir: true,
-            ignoreNotPermitted: true,
-            ignoreDirectoryPattern: /node_modules/
-        } ,function (monitor) {
-            monitor.files[path[0]+'/.zshrc'] // Stat object for my zshrc.
-            monitor.on("created", function (f, stat) {
-                // Handle new files
-                console.log('Directory', f, 'has been added');
-                if(showInLogFlag){
-                    addLog("Folder added : "+f,"new");
-                }
-            })
-            monitor.on("changed", function (f, curr, prev) {
-                // Handle file changes
-                console.log('Directory', f, 'has been changes');
-                if(showInLogFlag){
-                    addLog("Folder changes : "+f);
-                }
-            })
-            monitor.on("removed", function (f, stat) {
-                // Handle removed files
-                console.log('Directory', f, 'has been removed');
-                if(showInLogFlag){
-                    addLog("Folder removed : "+f);
-                }
-
-            })
-            onWatcherReady();
-        })
-    })
-}
-
-function StartChokidarWatcher(path){
-    document.getElementById("start").disabled = true;
-    document.getElementById("messageLogger").innerHTML = "Scanning the path, please wait ...";
-    watcher = chokidar.watch(path, {
-        ignored: /[\/\\]\./,
-        persistent: true,
-        ignorePermissionErrors: true
-    });
-    watcher
-        /*.on('add', function(path) {
-         console.log('File', path, 'has been added');
-         if(showInLogFlag){
-         addLog("File added : "+path,"new");
-         }
-         })
-         .on('addDir', function(path) {
-         console.log('Directory', path, 'has been added');
-         if(showInLogFlag){
-         addLog("Folder added : "+path,"new");
-         }
-         })*/
-        .on('change', function(path) {
-            console.log('File', path, 'has been changed');
-            if(showInLogFlag){
-                addLog("A change ocurred : "+path,"change");
-            }
-        })
-        .on('unlink', function(path) {
-            console.log('File', path, 'has been removed');
-            if(showInLogFlag){
-                addLog("A file was deleted : "+path,"delete");
-            }
-        })
-        .on('unlinkDir', function(path) {
-            console.log('Directory', path, 'has been removed');
-            if(showInLogFlag){
-                addLog("A folder was deleted : "+path,"delete");
-            }
-        })
-        .on('error', function(error) {
-            console.log('Error happened', error);
-            if(showInLogFlag){
-                addLog("An error ocurred: ","delete");
-                console.log(error);
-            }
-        })
-        .on('ready', onWatcherReady)
-    //.on('raw', function(event, path, details) {
-    // This event should be triggered everytime something happens.
-    //	console.log('Raw event info:', event, path, details);
-    //});
-}
-function StartFSMonitorWatcher(path){
-    document.getElementById("messageLogger").innerHTML = "Scanning the path, please wait ...";
-    require('du')(path, function (err, size) {
-        console.log('The size of' + path + ' is:', size, 'bytes')
-
-        var monitor = fsmonitor.watch(path, null);
-        monitor.on('change', function(changes) {
-            console.log(changes);
-
-            console.log(changes, 'has been added');
-            if(showInLogFlag){
-                addLog("Changed : "+changes,'change');
-            }
-        });
-        onWatcherReady();
-    })
-}
 function StartNodeWatchWatcher(path){
     document.getElementById("messageLogger").innerHTML = "Scanning the path, please wait ...";
     //require('du')(path, function (err, size) {
@@ -153,9 +44,6 @@ document.getElementById("start").addEventListener("click",function(e){
         properties: ['openDirectory']
     },function(path){
         if(path){
-            //StartChokidarWatcher(path[0]);
-            //StartFSMonitorWatcher(path[0]);
-            //StartWatchWatcher(path[0]);
             StartNodeWatchWatcher(path[0]);
         }else {
             console.log("No path selected");
@@ -199,24 +87,27 @@ function addPathToWatch(path){
             }
         }
         watchingList.push(path);
+        startToWatch(path);
         conf.set('towatch',watchingList);
     } else {
         conf.set('towatch',[path]);
     }
 }
-
+function startToWatch(filePath){
+    nodewatch(filePath ,{ recursive: true }, function(evt, name) {
+        nexttick(function(){
+            console.log('%s changed.', name);
+            reporter.report('file-event',JSON.stringify({'type':'changed','file':name}))
+            if(showInLogFlag){
+                addLog("Changed : "+name,'change');
+            }
+        })
+    });
+}
 var watchingList = conf.get('towatch')
 if(watchingList){
     watchingList.forEach(function(item){
         console.log('to watch: '+item)
-        nodewatch(item ,{ recursive: true }, function(evt, name) {
-            nexttick(function(){
-                console.log('%s changed.', name);
-                reporter.report('file-event',JSON.stringify({'type':'changed','file':name}))
-                if(showInLogFlag){
-                    addLog("Changed : "+name,'change');
-                }
-            })
-        });
+        startToWatch(item);
     })
 }
